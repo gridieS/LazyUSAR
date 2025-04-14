8f2d681023137110ee5446a9e930573df4f13112
gridieS <gridieworks@gmail.com>
2025-04-14 23:37:26 +0300

    Added MVP for UI, UI assets, and reworked __main__.py

--- LazyUSAR/__main__.py
+++ LazyUSAR/__main__.py
@@ -1,43 +1,14 @@
-import keyboard
-from . import jumping_jacks
-from time import sleep
-
-
-SLEEP_TIME = 0.1
-
-
-class UI:
-    def __init__(
-        self,
-        exit_key: str,
-    ):
-        self.__exit_key = exit_key
-        print(f"Press {exit_key} to exit")
-
-        self.running = True
-        keyboard.hook(self.on_key_event)
-
-        self.temp = jumping_jacks.JumpingJackController(
-            500, 1, 10, jumping_jacks.JumpingJackType.UPPERCASE, "!", False
-        )
-        self.temp.start()
-        self.block_until_stopped()
-
-    def block_until_stopped(self):
-        while self.running:
-            sleep(SLEEP_TIME)
-
-    def on_key_event(self, key: keyboard.KeyboardEvent):
-        if key.event_type != "down":
-            return
-        if key.name == self.__exit_key:
-            self.temp.exit()
-            self.running = False
+from . import jumping_jacks, ui
 
 
 def main():
     try:
-        UI("f6")
+        jumping_jacks_ui = ui.JumpingJackUI(
+            "f6",
+            "f5",
+        )
+        jumping_jacks_ui.start()
+
     except KeyboardInterrupt:
         return
 
