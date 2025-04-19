97edeb67d8c8bd26125fecd569f39d715439b921
gridieS <gridieworks@gmail.com>
2025-04-19 19:13:50 +0300

    Added MVP (without sending message in roblox)

--- LazyUSAR/script_playback.py
+++ LazyUSAR/script_playback.py
@@ -2,25 +2,97 @@
 # For each i, copy array[i] to clipboard
 # Enter chat, paste from clipboard
 # Do until done
+# from .counter_types import ControlledIntervaledCounter
+# from . import system_controller
+import system_controller
+from counter_types import ControlledIntervaledCounter
+from time import sleep
 
 
 class ScriptPlaybackController:
+    def _initialize(
+        self,
+        end_callback: callable,
+        playback_callback: callable,
+        interval: int,
+        script: str,
+        username: str,
+        rank: str,
+    ):
+        self.interval = interval
+        script = script.replace("<username>", username)
+        script = script.replace("<rank>", rank)
+        self.script_array = list(
+            filter(lambda line: line.strip() != "", script.splitlines())
+        )
+        self.username = username
+        self.rank = rank
+        self.playback_callback = playback_callback
+        self.end_callback = end_callback
+        self.counter = ControlledIntervaledCounter(
+            self._send_script_line,
+            0,
+            self.interval,
+            stop_count=len(self.script_array) - 1,
+            backward_key="j",
+            toggle_key="k",
+            forward_key="l",
+            step=1,
+        )
+
     def __init__(
         self,
         end_callback: callable,
+        playback_callback: callable,
+        interval: int,
+        script: str,
+        username: str,
+        rank: str,
     ):
-        self.islistening = False
-        self.end_callback = end_callback
+        self._initialize(
+            end_callback, playback_callback, interval, script, username, rank
+        )
+
+    def remake(
+        self,
+        interval: int,
+        script: str,
+        username: str,
+        rank: str,
+    ):
+        self.exit()
+        self._initialize(
+            self.end_callback, self.playback_callback, interval, script, username, rank
+        )
+
+    def _send_script_line(self, line_num: int):
+        print(f"Sending script line: {self.script_array[line_num]}")
+        self.playback_callback(self.script_array[line_num])
+        if line_num + 1 > len(self.script_array):
+            self.reset()
+            self.end_callback()
 
     def start(self):
-        print("Started")
+        if len(self.script_array) == 0:
+            print("No script lines to send.")
+            self.end_callback()
+            return
+        system_controller.running = True
+        self.counter.start()
 
     def stop(self):
         self.exit()
 
     def exit(self):
-        print("Exited")
+        system_controller.running = False
+        self.counter.exit()
 
     def toggle(self) -> bool:
-        self.islistening = not self.islistening
-        return self.islistening
+        if len(self.script_array) == 0:
+            return False
+        if self.counter.is_listening:
+            self.stop()
+            return False
+        else:
+            self.start()
+            return True
